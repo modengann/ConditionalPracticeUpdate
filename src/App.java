bc450276aa29ca3cdfdece56bc0c0689cc1070b6
CaseyGann <cmoden@gannacademy.org>
2023-09-11 10:38:38 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,18 @@
+/** Overall task: Take a number from the user and decide if it meets a set of criteria
+ *  
+ *  1. Ask the user for a number
+ *  2. Based on the user input, follow these rules:
+ *      -If the number is divisible by 2 and 3, print "DivByTwoAndThree".
+ *      -If the number is divisible by 2 but not by 3, print "DivByTwo".
+ *      -If the number is divisible by 3 but not by 2, print "DivByThree".
+ *      -If the number is not divisible by either 2 or 3, print the number itself.
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
